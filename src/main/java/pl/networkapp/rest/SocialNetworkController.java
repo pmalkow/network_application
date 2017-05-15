76afbf7a17133ab0015cec627c4695033a72991d
pmalkow <pmalkowiak@gmail.com>
2017-05-15 09:10:44 +0200

    Introduce logging

--- src/main/java/pl/networkapp/rest/SocialNetworkController.java
+++ src/main/java/pl/networkapp/rest/SocialNetworkController.java
@@ -1,5 +1,7 @@
 package pl.networkapp.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import static pl.networkapp.domain.MessageValidator.isInvalid;
 @RestController
 class SocialNetworkController {
 
+	private static final Logger logger = LoggerFactory.getLogger(SocialNetworkController.class);
+
 	@Autowired private UserRepository userRepository;
 	@Autowired private FeedProvider feedProvider;
 
@@ -22,28 +26,34 @@ class SocialNetworkController {
 	ResponseEntity<Void> postMessage(@RequestHeader(value = USER_ID_HEADER) String userId,
 	                                        @RequestBody String message) {
 		if (isInvalid(message)) {
+			logger.error("Message to post is invalid.");
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 		userRepository.postMessage(userId, message);
+		logger.info("Successfully posted a message for user: {}", userId);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = "/wall", method = RequestMethod.GET)
 	ResponseEntity<List<String>> getMyMessages(@RequestHeader(value = USER_ID_HEADER) String userId) {
+		logger.info("Fetching messages for user: {}", userId);
 		return new ResponseEntity<>(feedProvider.getUsersFeed(userId), HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/follow/{user}", method = RequestMethod.POST)
 	ResponseEntity<Void> follow(@RequestHeader(value = USER_ID_HEADER) String userId, @PathVariable String user) {
 		if (userToFollowDoesNotExist(user)) {
+			logger.error("User to follow does not exist: {}", user);
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		userRepository.follow(userId, user);
+		logger.info("User {} successfully followed {}", userId, user);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = "/followingFeed", method = RequestMethod.GET)
 	ResponseEntity<List<String>> getFollowingFeed(@RequestHeader(value = USER_ID_HEADER) String userId) {
+		logger.info("Fetching followed users feeds for user: {}", userId);
 		return new ResponseEntity<>(feedProvider.getFollowingFeed(userId), HttpStatus.OK);
 	}
 
